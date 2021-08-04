98d0d0f7f9fa8aca9e70dd5268ea9b9b5b741129
unknown <mdhabibulbasarshohag34@gmail.com>
2021-08-04 19:38:18 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,146 @@
+function myFunc() {
+    //dts1
+    document.getElementById('bts1').style.width = '25px';
+    document.getElementById('bts1').style.height = '500px';
+    document.getElementById('bts1').style.transition = '5s';
+    document.getElementById('bts1').style.position = 'aboslute';
+    document.getElementById('bts1').style.top = '100px';
+    document.getElementById('bts1').style.left = '200px';
+    document.getElementById('bts1').style.borderRadius = '0';
+    //bts2
+    document.getElementById('bts2').style.width = '200px';
+    document.getElementById('bts2').style.height = '25px';
+    document.getElementById('bts2').style.transition = '5s';
+    document.getElementById('bts2').style.position = 'aboslute';
+    document.getElementById('bts2').style.top = '100px';
+    document.getElementById('bts2').style.left = '200px';
+    document.getElementById('bts2').style.borderRadius = '0';
+    //bts3
+    document.getElementById('bts3').style.width = '200px';
+    document.getElementById('bts3').style.height = '25px';
+    document.getElementById('bts3').style.transition = '5s';
+    document.getElementById('bts3').style.position = 'aboslute';
+    document.getElementById('bts3').style.top = '350px';
+    document.getElementById('bts3').style.left = '200px';
+    document.getElementById('bts3').style.borderRadius = '0';
+    //bts4
+    document.getElementById('bts4').style.width = '200px';
+    document.getElementById('bts4').style.height = '25px';
+    document.getElementById('bts4').style.transition = '5s';
+    document.getElementById('bts4').style.position = 'aboslute';
+    document.getElementById('bts4').style.top = '600px';
+    document.getElementById('bts4').style.left = '200px';
+    document.getElementById('bts4').style.borderRadius = '0';
+    //bts5
+    document.getElementById('bts5').style.width = '50px';
+    document.getElementById('bts5').style.height = '25px';
+    document.getElementById('bts5').style.transition = '5s';
+    document.getElementById('bts5').style.position = 'aboslute';
+    document.getElementById('bts5').style.top = '114px';
+    document.getElementById('bts5').style.left = '382px';
+    document.getElementById('bts5').style.borderRadius = '0';
+    //bts6
+    document.getElementById('bts6').style.width = '50px';
+    document.getElementById('bts6').style.height = '25px';
+    document.getElementById('bts6').style.transition = '5s';
+    document.getElementById('bts6').style.position = 'aboslute';
+    document.getElementById('bts6').style.top = '336px';
+    document.getElementById('bts6').style.left = '382px';
+    document.getElementById('bts6').style.borderRadius = '0';
+    //bts7
+    document.getElementById('bts7').style.width = '25px';
+    document.getElementById('bts7').style.height = '205px';
+    document.getElementById('bts7').style.transition = '5s';
+    document.getElementById('bts7').style.position = 'aboslute';
+    document.getElementById('bts7').style.top = '134px';
+    document.getElementById('bts7').style.left = '408px';
+    document.getElementById('bts7').style.borderRadius = '0';
+    //bts8
+    document.getElementById('bts8').style.width = '50px';
+    document.getElementById('bts8').style.height = '25px';
+    document.getElementById('bts8').style.transition = '5s';
+    document.getElementById('bts8').style.position = 'aboslute';
+    document.getElementById('bts8').style.top = '366px';
+    document.getElementById('bts8').style.left = '382px';
+    document.getElementById('bts8').style.borderRadius = '0';
+    //bts9
+    document.getElementById('bts9').style.width = '50px';
+    document.getElementById('bts9').style.height = '25px';
+    document.getElementById('bts9').style.transition = '5s';
+    document.getElementById('bts9').style.position = 'aboslute';
+    document.getElementById('bts9').style.top = '585px';
+    document.getElementById('bts9').style.left = '382px';
+    document.getElementById('bts9').style.borderRadius = '0';
+    //bts10
+    document.getElementById('bts10').style.width = '25px';
+    document.getElementById('bts10').style.height = '205px';
+    document.getElementById('bts10').style.transition = '5s';
+    document.getElementById('bts10').style.position = 'aboslute';
+    document.getElementById('bts10').style.top = '386px';
+    document.getElementById('bts10').style.left = '408px';
+    document.getElementById('bts10').style.borderRadius = '0';
+    //bts11
+    document.getElementById('bts11').style.width = '250px';
+    document.getElementById('bts11').style.height = '25px';
+    document.getElementById('bts11').style.transition = '5s';
+    document.getElementById('bts11').style.position = 'aboslute';
+    document.getElementById('bts11').style.top = '100px';
+    document.getElementById('bts11').style.left = '500px';
+    document.getElementById('bts11').style.borderRadius = '0';
+    //bts12
+    document.getElementById('bts12').style.width = '25px';
+    document.getElementById('bts12').style.height = '525px';
+    document.getElementById('bts12').style.transition = '5s';
+    document.getElementById('bts12').style.position = 'aboslute';
+    document.getElementById('bts12').style.top = '100px';
+    document.getElementById('bts12').style.left = '615px';
+    document.getElementById('bts12').style.borderRadius = '0';
+    //bts13
+    document.getElementById('bts13').style.width = '75px';
+    document.getElementById('bts13').style.height = '25px';
+    document.getElementById('bts13').style.transition = '5s';
+    document.getElementById('bts13').style.position = 'aboslute';
+    document.getElementById('bts13').style.top = '600px';
+    document.getElementById('bts13').style.left = '589px';
+    document.getElementById('bts13').style.borderRadius = '0';
+    //bts14
+    document.getElementById('bts14').style.width = '250px';
+    document.getElementById('bts14').style.height = '25px';
+    document.getElementById('bts14').style.transition = '5s';
+    document.getElementById('bts14').style.position = 'aboslute';
+    document.getElementById('bts14').style.top = '100px';
+    document.getElementById('bts14').style.left = '800px';
+    document.getElementById('bts14').style.borderRadius = '0';
+    //bts15
+    document.getElementById('bts15').style.width = '250px';
+    document.getElementById('bts15').style.height = '25px';
+    document.getElementById('bts15').style.transition = '5s';
+    document.getElementById('bts15').style.position = 'aboslute';
+    document.getElementById('bts15').style.top = '350px';
+    document.getElementById('bts15').style.left = '800px';
+    document.getElementById('bts15').style.borderRadius = '0';
+    //bts16
+    document.getElementById('bts16').style.width = '250px';
+    document.getElementById('bts16').style.height = '25px';
+    document.getElementById('bts16').style.transition = '5s';
+    document.getElementById('bts16').style.position = 'aboslute';
+    document.getElementById('bts16').style.top = '600px';
+    document.getElementById('bts16').style.left = '800px';
+    document.getElementById('bts16').style.borderRadius = '0';
+    //bts17
+    document.getElementById('bts17').style.width = '25px';
+    document.getElementById('bts17').style.height = '250px';
+    document.getElementById('bts17').style.transition = '5s';
+    document.getElementById('bts17').style.position = 'aboslute';
+    document.getElementById('bts17').style.top = '100px';
+    document.getElementById('bts17').style.left = '800px';
+    document.getElementById('bts17').style.borderRadius = '0';
+    //bts18
+    document.getElementById('bts18').style.width = '25px';
+    document.getElementById('bts18').style.height = '250px';
+    document.getElementById('bts18').style.transition = '5s';
+    document.getElementById('bts18').style.position = 'aboslute';
+    document.getElementById('bts18').style.top = '350px';
+    document.getElementById('bts18').style.left = '1025px';
+    document.getElementById('bts18').style.borderRadius = '0';
+}
